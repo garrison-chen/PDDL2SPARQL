d4cf4422f408be4c8886bc02ef3ef5c73516ea6c
Guangyi Chen <44410011+guangyichen0802@users.noreply.github.com>
2021-07-13 17:46:05 +0200

    test connection

--- src/PDDL2SPARQL/PDDL2SPARQL.java
+++ src/PDDL2SPARQL/PDDL2SPARQL.java
@@ -1,7 +1,7 @@
 package PDDL2SPARQL;
 
-import java.io.File;
-import java.io.FileWriter;
+//import java.io.File;
+//import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
